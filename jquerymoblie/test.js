9da659e8b950928eef287ec080baff7fb5dc6bda
wynnlee <457651143@qq.com>
2017-03-25 21:42:24 +0800

    Add files via upload

---
+++ jquerymoblie/test.js
@@ -0,0 +1,28 @@
+ // function isload(){
+// 	var message,len,upper;
+// 	message="helloworld!";
+// 	alert(message);
+// 	len=message.length;
+// 	alert(len);
+// 	Upper=message.toUpperCase();
+// 	alert(Upper);
+// }
+
+// function mOver(obj)
+// {
+// 	obj.innerHTML="hello";
+
+// }
+// function mOut(obj)
+// {
+// 	obj.innerHTML="把鼠标移到上面啊";
+	
+// }
+
+
+
+$(
+	function(){
+		$("#mydiv").css({color:#666666; background:"blue";})
+    }
+)
